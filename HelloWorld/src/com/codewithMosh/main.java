a1855da35c5b00981dc85ed9ada207acdb674ac2
Jackie <JGtheophila19@gmail.com>
2020-05-22 14:41:30 -0400

    creating variable types

--- HelloWorld/src/com/codewithMosh/main.java
+++ HelloWorld/src/com/codewithMosh/main.java
@@ -2,6 +2,16 @@ package com.codewithMosh;
 
 public class main {
 	public static void main (String[] args){
-		System.out.println("Hello Wolrd");
+		
+		int temperature = 20;
+		System.out.println(temperature);
+		
+		int age = 30;
+		age = 35;
+		System.out.println(age);
+		
+		int myAge = 30;
+		int herAge = myAge;
+		System.out.println(herAge);
 	}
 }
