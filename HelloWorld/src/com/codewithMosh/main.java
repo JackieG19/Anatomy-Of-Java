c97bd329512bcd84a8532ad1c79026d29fdda42f
Jackie <JGtheophila19@gmail.com>
2020-05-23 13:45:42 -0400

    Primitive Type vs Reference Type

--- HelloWorld/src/com/codewithMosh/main.java
+++ HelloWorld/src/com/codewithMosh/main.java
@@ -4,8 +4,18 @@ import java.util.Date;
 
 public class main {
 	public static void main (String[] args){
-		byte age = 30;
-		Date now = new Date();
-		System.out.println(now);
+		// Primitive Type
+		byte x = 1;
+		byte y = x;
+		x = 2;
+		System.out.println(y);
+		// the bytes are in different memory location which makes them independent from each other 
+		
+		// Reference Type
+		Point point1 = new Point(1, 1);
+		Point point2 = point1;
+		point1.x = 2;
+		System.out.println(point2);
+		// the point value store the address of the point memory
 	}
 }
