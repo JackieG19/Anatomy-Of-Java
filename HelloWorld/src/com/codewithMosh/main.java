1317209acdce527f28520e852f4bba10a62bfdae
Jackie <JGtheophila19@gmail.com>
2020-05-24 14:32:53 -0400

    using multi-dimensional arrays with numbers

--- HelloWorld/src/com/codewithMosh/main.java
+++ HelloWorld/src/com/codewithMosh/main.java
@@ -4,15 +4,12 @@ import java.util.Arrays;
 
 public class main {
 	public static void main (String[] args){
-		int[] numbers = new int[5];
-		numbers[0] = 1;
-		numbers[1] = 2;
+		int[][] numbers = new int[2][3];
+		numbers[0][0] = 1;
 		System.out.println(Arrays.toString(numbers));
+		System.out.println(Arrays.deepToString(numbers));
 		
-		int[] number = { 2, 3, 5, 1, 4 };
-		System.out.println(number.length);
-		
-		Arrays.sort(number);
-		System.out.println(Arrays.toString(number));
+		int[][] number = {{1, 2, 3}, {4, 5, 6}};
+		System.out.println(Arrays.deepToString(number));
 	}
 }
