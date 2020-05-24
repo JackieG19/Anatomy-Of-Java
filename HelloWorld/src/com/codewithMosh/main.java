7eaca88979feed7ee740516b4cab7d1d282000a0
Jackie <JGtheophila19@gmail.com>
2020-05-24 13:57:42 -0400

    using methods of the string class

--- HelloWorld/src/com/codewithMosh/main.java
+++ HelloWorld/src/com/codewithMosh/main.java
@@ -1,21 +1,16 @@
 package com.codewithMosh;
 
-import java.awt.Point;
-
 public class main {
 	public static void main (String[] args){
-		// Primitive Type
-		byte x = 1;
-		byte y = x;
-		x = 2;
-		System.out.println(y);
-		// the bytes are in different memory location which makes them independent from each other 
+		String message = "Hello World";
+		System.out.println(message);
 		
-		// Reference Type
-		Point point1 = new Point(1, 1);
-		Point point2 = point1;
-		point1.x = 2;
-		System.out.println(point2);
-		// the point value store the address of the point memory
+		String message2 = "Hello World" + "!!";	
+		System.out.println(message2.endsWith("!!"));
+		System.out.println(message2.startsWith("!!"));
+		
+		System.out.println(message2.length());
+		System.out.println(message2.indexOf("e"));
+		System.out.println(message2.replace("!", "*"));
 	}
 }
