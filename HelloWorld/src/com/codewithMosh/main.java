5e06d130b6328b947e07a2c93db8b40f043a11b2
Jackie <JGtheophila19@gmail.com>
2020-05-25 14:41:25 -0400

    using order of operations

--- HelloWorld/src/com/codewithMosh/main.java
+++ HelloWorld/src/com/codewithMosh/main.java
@@ -2,36 +2,10 @@ package com.codewithMosh;
 
 public class main {
 	public static void main (String[] args){
-		int result = 10 + 3;
+		int result = (10 + 3) * 2;
 		System.out.println(result);
 		
-		int result1 = 10 * 3;
-		System.out.println(result1);
-		
-		double result2 = (double)10 / (double)3;
-		System.out.println(result2);
-		
-		int x = 1;
-		x++;
-		int y = 1;
-		System.out.println(x);
-		System.out.println(y);
-		
-		int b = 1;
-		//b = b + 2;
-		b += 2;
-		System.out.println(b);
-		
-		int c = 1;
-		c -= 2;
-		System.out.println(c);
-		
-		int d = 1;
-		d *= 2;
-		System.out.println(d);
-		
-		int e = 1;
-		e /= 2;
-		System.out.println(e);
+		double value = 2 * (10 + 3) * 5;
+		System.out.println(value);
 	}
 }
